24b25b5721749312cc915aabc4cbd1065dd0bca4
Arithmophobia <sannan.kanny@gmail.com>
2018-09-22 14:18:30 +0300

    Lisää tehtävä 3.9

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors())
 
 morgan.token('data', function getData (req) {
   return req.data
